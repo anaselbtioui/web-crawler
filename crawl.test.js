7d18322540ee1246df1bd103b393a9fa0a0af250
Anas ELBTIOUI <anaselbtioui@gmail.com>
2024-09-25 20:31:17 +0100

    test

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL, strip protocol", () => {
@@ -26,5 +26,22 @@ test("normalizeURL, strip http", () => {
     const input = "http://BLOG.boot.dev/path/";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML", () => {
+    const input = `
+    <html>  
+        <body>
+            <a href="https://blog.boot.dev">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+
+    const inputBaseUrl = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(input, inputBaseUrl);
+    const expected = ["https://blog.boot.dev/"];
     expect(actual).toEqual(expected);
 });
